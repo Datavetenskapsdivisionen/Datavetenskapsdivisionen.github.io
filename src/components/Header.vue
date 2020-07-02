96ccab922d1bb823bf7e679ee0b9deb664a96b55
oskery <oskar.lyrstrand@gmail.com>
2020-07-02 14:21:25 +0200

    Update header with correct pages

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,8 @@
     </strong>
     <nav class="nav">
       <g-link class="nav__link" to="/">Start</g-link>
-      <g-link class="nav__link" to="/styrelsen/">Styrelsen</g-link>
-      <g-link class="nav__link" to="/lek/">LeK</g-link>
+      <g-link class="nav__link" to="/mottagningen-2020/">Mottagningen 2020</g-link>
+      <g-link class="nav__link" to="/gdpr/">Integritetspolicy</g-link>
       <g-link class="nav__link" to="/kontakt/">Kontakt</g-link>
     </nav>
   </header>
