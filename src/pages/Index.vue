e1d5dde3a172011c996165983e1047d890c026af
oskery <oskar.lyrstrand@gmail.com>
2020-07-04 14:33:45 +0200

    Fix meta title

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,12 +4,23 @@
   </Layout>
 </template>
 
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.allStaticPage.edges[0].node.title
+    }
+  }
+}
+</script>
+
 <page-query>
 query {
   allStaticPage(filter: {order: {eq: 0}}) {
     edges {
       node {
         content
+        title
       }
     }
   }
--- src/templates/StaticPage.vue
+++ src/templates/StaticPage.vue
@@ -4,10 +4,21 @@
   </Layout>
 </template>
 
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.staticPage.title
+    }
+  }
+}
+</script>
+
 <page-query>
 query Page ($id: ID!) {
   staticPage: staticPage (id: $id) {
-    content
+    content,
+    title
   }
 }
 </page-query>
